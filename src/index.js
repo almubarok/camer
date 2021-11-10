349d11d80f48b85aed755c05a5de9d59a8af54dd
Much Arafat Al Mubarok <almubarokarafat@gmail.com>
2021-11-10 18:08:41 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import './index.css';
-import App from './UseEffect';
-// import Todo from './todo';
-import Pokemon from './Pokemon';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-// Navbar
-// import Navbar from './components/Navbar';
-
-// // Pages
-// import Home from './pages/index';
-// import Contact from './pages/contacts';
-// import ContactMe from './pages/contactme';
-// import Blog from './pages/blog';
-// import NotFound from './pages/notfound';
-// import Redirect from './pages/redirect';
-// import Hook from './pages/hook';
-
-
-import Routing from './routing';
-
+import Routing from "./routing";
 
 ReactDOM.render(
-    <BrowserRouter>
-      <Routing/>
-    </BrowserRouter>
-
-
-  // <React.StrictMode>
-    // <App />
-    // <Todo/>
-    // <Pokemon/>
-  // </React.StrictMode>
-  ,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Routing />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
