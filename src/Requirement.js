52b625ba676a99da0a059ecc70d20f3f3344f2c2
Much Arafat Al Mubarok <almubarokarafat@gmail.com>
2021-11-10 11:54:44 +0700

    add req

---
+++ src/Requirement.js
@@ -0,0 +1,40 @@
+import "./App.css";
+
+import { useState } from "react";
+
+function Requirement() {
+  const [counter, setCounter] = useState(0);
+  const [amount, setAmount] = useState(1);
+  const counterStyle = {
+    color: counter >= 100 ? "green" : counter < 0 ? "red" : "",
+  };
+  return (
+    <div className="App">
+      <h3 data-testid="header">My Counter</h3>
+      <p style={counterStyle} data-testid="counter">
+        {counter}
+      </p>
+
+      <button
+        data-testid="btnDecrement"
+        onClick={() => setCounter(counter - amount)}
+      >
+        -
+      </button>
+      <input
+        data-testid="inputAmount"
+        style={{ textAlign: "center" }}
+        value={amount}
+        onChange={(e) => setAmount(parseInt(e.target.value))}
+      />
+      <button
+        data-testid="btnIncrement"
+        onClick={() => setCounter(counter + amount)}
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+export default Requirement;
