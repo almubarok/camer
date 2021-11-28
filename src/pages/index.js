9cbaed4c53c7c3ef8b3d5a790ff6055f0db0278c
Much Arafat Al Mubarok <almubarokarafat@gmail.com>
2021-11-28 22:09:27 +0700

    v0.0.1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,28 @@
 import useGetUsers from "hooks/useGetUsers";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function Index() {
   const { users } = useGetUsers();
+  const navigate = useNavigate();
 
+  const handleClick = (id) => {
+    navigate("/todolist/" + id);
+  };
   return (
     <div>
       {users?.map((v) => {
         return (
-          <p key={v.id}>
-            <Link to={`todolist/${v.id}`}>{v.name}</Link>
-          </p>
+          <>
+            {/* pakai onClick bisa */}
+            <p key={v.id} onClick={() => handleClick(v.id)}>
+              {v.name}
+            </p>
+
+            {/* pakai Link bisa */}
+            <p key={v.id}>
+              <Link to={`todolist/${v.id}`}>{v.name}</Link>
+            </p>
+          </>
         );
       })}
     </div>
